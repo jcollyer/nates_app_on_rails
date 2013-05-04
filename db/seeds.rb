edab2a4f6e9713fa8d644e9c8ad6480bde27638d
Jeremy Collyer <collyerdesign@gmail.com>
2013-05-04 13:02:19 -0700

    Add Seed for Lessons

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require Rails.root.join("spec", "blueprints.rb")
+
+8.times do
+	Lesson.make!
+end
