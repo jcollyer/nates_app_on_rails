931663be140692bb61e31e80438ac443c846c7d3
Jeremy Collyer <collyerdesign@gmail.com>
2013-05-04 12:11:56 -0700

    Refactor lessons_controller_spec

--- spec/controllers/api/lessons_controller_spec.rb
+++ spec/controllers/api/lessons_controller_spec.rb
@@ -1,34 +1,39 @@
 require 'spec_helper'
 
 describe Api::LessonsController do
+	let!(:lesson) { Lesson.make!(:title => "Foo Title") }
+
+	# Note: This could be moved to spec_helper so that it could be used everywhere.
+	def decoded_body
+		# ActiveSupport::JSON.decode turns a json string back into a ruby hash.
+		ActiveSupport::JSON.decode(response.body)
+	end
+
 	describe "GET to :index" do
-		it "responds ok" do
+		before do
 			get :index
+		end
+
+		it "responds ok" do
 			expect(response).to be_ok
 		end
 
 		it "returns lessons json" do
-			Lesson.make!(:title => "Foo Title")
-			get :index
-			# ActiveSupport::JSON.decode turns a json string back into a ruby hash.
-			decoded_body = ActiveSupport::JSON.decode(response.body)
 			expect(decoded_body["lessons"]).to be_present
 		end
 	end
 
 	describe "GET to :show" do
-		it "responds ok" do
-			lesson = Lesson.make!(:title => "Foo Title")
+		before do
 			get :show, :id => lesson.id
+		end
+
+		it "responds ok" do
 			expect(response).to be_ok
 		end
 
 		it "returns lesson hash" do
-			lesson = Lesson.make!(:title => "Foo Title")
-			get :show, :id => lesson.id
-			decoded_body = ActiveSupport::JSON.decode(response.body)
 			expect(decoded_body["lesson"]).to be_present
 		end
 	end
-
 end
