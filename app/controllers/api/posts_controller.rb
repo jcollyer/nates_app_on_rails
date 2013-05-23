b971fb7843d7c0eb597376cc94779047bc453dd6
jcollyer <collyerdesign@gmail.com>
2013-05-23 08:23:55 -0700

    Add CRUD for posts

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -9,4 +9,26 @@ class Api::PostController < ApplicationController
     post = Post.find(params[:id])
     render :json => Api::PostPresenter.new(post)
   end
+
+  def create
+    post = Post.create! do |instance|
+      instance.title = params[:post][:title]
+      instance.summary = params[:post][:summary]
+    end
+    render :json => Api::PostPresenter.new(post), :status => :created
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.title = params[:post][:title]
+    post.summary = params[:post][:summary]
+    post.save!
+    render :json => Api::PostPresenter.new(post), :status => :accepted
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    render :nothing => true, :status => :accepted
+  end
 end
