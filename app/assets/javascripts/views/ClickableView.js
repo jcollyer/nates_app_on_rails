36665cbad02ef09207ac74ada38e5102aeafb8c6
jcollyer <collyerdesign@gmail.com>
2013-07-07 13:31:02 -0700

    revamp Top-Nav.

--- app/assets/javascripts/views/ClickableView.js
+++ app/assets/javascripts/views/ClickableView.js
@@ -29,3 +29,19 @@ App.EditBiblebook = Ember.View.extend({
     //alert(URL);
   }
 });
+
+App.ShowNav = Ember.View.extend({
+  click: function() {
+    $(".hamburger").css("display", "none");
+    $(".the_top_nav").css("position", "relative");
+    $(".the_top_nav").animate({marginTop: "0px" }, 500);
+    }
+});
+
+App.HideNav = Ember.View.extend({
+  click: function() {
+    $(".the_top_nav").css("margin-top", "-140px");
+    $(".the_top_nav").css("position", "absolute");
+    $(".hamburger").css("display", "block");
+    }
+});
